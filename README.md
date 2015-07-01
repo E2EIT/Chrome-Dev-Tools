aa18f0e0296864547f5fb87de7149d55385a6dd3
Robert <E2EIT@users.noreply.github.com>
2015-06-30 17:49:57 -0700

    v0.1: Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chrome-Dev-Tools
+
+This is a simple HTML, CSS and JavaScript project for demoing the Chrome Dev Tools.
---
+++ js/index.js
@@ -0,0 +1,138 @@
+var Model = {
+	/* The Model handles our data. */
+
+	// Our model has a single ToDoItem array
+	ToDoItemArray: [],
+
+	// ToDoItem Factory
+	ToDoItem: function newToDoItem(id, title) {
+		return {
+			id: id,
+			title: title,
+			complete: false
+		}
+	},
+
+	toggleItemComplete: function toggleItemComplete(index) {
+		/**
+		 * Sets the To Do item at 'index' to complete.
+		 * @index: integer
+		 */
+		Model.ToDoItemArray[index].complete = !Model.ToDoItemArray[index].complete;
+	}
+};
+
+var View = {
+	// The View handles changes to our view (DOM)
+
+	getElementById: function getElementById(id) {
+		return document.getElementById(id);
+	},
+
+	updateToDoCount: function updateToDoCount() {
+		var itdCntElm = View.getElementById('itdCnt');
+		var itdCnt = 0;
+		Model.ToDoItemArray.forEach(function (toDoItem, index) {
+			if (!toDoItem.complete) ++itdCnt;
+		});
+		// Clear the current HTML list
+		itdCntElm.innerHTML = '' + itdCnt;
+	},
+
+	addToDoItem: function addToDoItem(toDoItem) {
+		var toDoListElm = View.getElementById('ulToDoList');
+		var cbId = 'cb_' + toDoItem.id;
+		var liId = 'li_' + toDoItem.id;
+		var classList = '';
+		if (toDoItem.complete) {
+			classList += ' todo-complete';
+
+		}
+
+		var itemHtml
+			= '<li id="' + liId + '" class="topcoat-list__item">\n    '
+			+ '	<label class="topcoat-checkbox ' + classList + '">\n'
+			+ '		<input id=' + cbId + ' type="checkbox" '
+			+ '			onclick="Controller.toggleItemComplete(' + toDoItem.id + ')">\n'
+			+ '			<div class="topcoat-checkbox__checkmark"></div>\n'
+			+ '			<span>' + toDoItem.title + '</span>'
+			+ '	</label>\n'
+			+ '</li>\n';
+
+		// Add the new item to the end of the list
+		toDoListElm.insertAdjacentHTML('beforeend', itemHtml);
+		// Update the To Do counts.
+		View.updateToDoCount();
+	},
+
+	getInputText: function getInputText() {
+		var inputElem = View.getElementById('titleInput');
+		return inputElem.value;
+	},
+
+	resetInput: function resetInput() {
+		var inputElm = View.getElementById('titleInput');
+		inputElm.value = null;
+		inputElm.focus();
+		var addBtnElm = View.getElementById('addBtn');
+		addBtnElm.disabled = true;
+	}
+};
+
+var Controller = {
+	/* The Controller handles the App logic */
+
+// Controller initialization
+	init: function init() {
+		// Add a click listener to our Add button
+		var addBtnElm = View.getElementById('addBtn');
+		/*addBtnElm.addEventListener('click',
+		 Controller.addToDoItem, false);*/
+		addBtnElm.onclick = Controller.addNewToDoItem;
+		// Start with the Add button disabled
+		addBtnElm.disabled = true;
+
+		// Add a checks for valid input state (non-empty)
+		var inputElm = View.getElementById('titleInput');
+		inputElm.onkeyup = function () {
+			// If the input field has a value, enable the Add button
+			addBtnElm.disabled = !inputElm.value;
+		};
+		inputElm.oninvalid = function () {
+			addBtnElm.disabled = true;
+		}
+	},
+
+	addNewToDoItem: function addNewToDoItem() {
+		var id = Model.ToDoItemArray.length;
+		var title = View.getInputText();
+		var newToDo = new Model.ToDoItem(id, title);
+		// Add new item to the array
+		Model.ToDoItemArray.push(newToDo);
+		// Clear the input field
+		View.resetInput();
+		// Update the view's list
+		//View.reloadToDoList();
+		View.addToDoItem(newToDo);
+	},
+
+
+	toggleItemComplete: function toggleItemComplete(index) {
+		/**
+		 * Remove a To Do from the list.
+		 * @indexString = index as a string
+		 **/
+
+		// We still have a string. We need an integer.
+		//var index = parseInt(indexString);
+
+		// We can finally remove the correct To Do item.
+		Model.toggleItemComplete(index);
+
+		// Tell the View to refresh the list
+		View.updateToDoCount();
+	}
+};
+
+Controller.init();
+
